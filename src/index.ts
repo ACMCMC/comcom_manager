a5efe067da47e27fbb8a3641f2c7f2229f53375b
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-12 18:04:42 +0100

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
-const port= 8080;
+const port= 443;
 
 app.get( "/prueba", (req: Request, res: Response) => {
     res.status(200).send("Hola mundo!");
 } );
+
+app.post("/action-endpoint", (req: Request, res: Response) => {
+    if (req.body["type"]==="url_verification") {
+        res.send(req.body["challenge"]);
+    }
+});
 
 app.listen(port, () => console.log("Express iniciado"));
