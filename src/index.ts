2df1a029a3211d3896606fcc9f83c66b94605181
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-12 18:20:08 +0100

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,10 @@ app.get( "/prueba", (req: Request, res: Response) => {
 
 app.post("/action-endpoint", (req: Request, res: Response) => {
     if (req.body["type"]==="url_verification") {
-        console.log(req.body["token"]);
         res.status(200).send(req.body["challenge"]);
     }
+    console.log(req);
+    res.status(200).send("No soportado");
 });
 
 app.listen(port, () => console.log("Express iniciado"));
