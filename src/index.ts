e8ff6a01bc773ba86987dba029430abab3e0ae7a
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-12 17:22:59 +0100

    Primera versión

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const port= 8080;
+
+app.get( "/", (req: Request, res: Response) => {
+    res.status(200).send("Hola mundo!");
+} );
+
+app.listen(port, () => console.log("Express iniciado"));
