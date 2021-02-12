83a6dfc4f3c2fa5bb65c06f34307cddbd9bc6b36
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-12 20:30:58 +0100

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.post('/action-endpoint', (req: Request, res: Response) => {
                 text: 'Hola! Aquí estoy, ' + '<@' + req.body['user'] + '>!'
             };
 
-            const resp: Promise<any> = axios.post('slack.com/api/chat.postMessage', body, options);
+            const resp: Promise<any> = axios.post('https://slack.com/api/chat.postMessage', body, options);
             resp.catch((err) => console.error(err)).then((resp) => console.error(resp));
         }
     }
