095ce445212add4287f6d7980b0dd4fadc70b004
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-13 02:03:52 +0100

    Heroku

--- src/resources/views.ts
+++ src/resources/views.ts
@@ -1,44 +1,58 @@
 import { View } from '@slack/web-api';
 
 const vistasBienvenida: View = {
-    type: 'home',
-
-    "blocks": [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "¡Hola! Soy el *Gestor de ComCom*. Habla conmigo para pedirle al Comité de Comunicación que difundan tus eventos. Tienes dos formas de hacerlo:"
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*1️⃣ Usar el comando `/enviar_evento` *. Este comando puedes usarlo desde cualquier lugar."
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*2️⃣ Hablar directamente conmigo*. Abajo encontrarás un botón que te permite realizar la misma acción que con `/enviar_evento`."
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "context",
-            "elements": [
-                {
-                    "type": "mrkdwn",
-                    "text": "👀 Puedes ver otros comandos con `/task list`.\n❓Usa `/task help` para obtener ayuda."
-                }
-            ]
-        }
-    ]
-
-};
+	"type": "home",
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "¡Hola! Soy el *Gestor de ComCom*. Habla conmigo para pedirle al Comité de Comunicación que difundan tus eventos. Tienes dos formas de hacerlo:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*1️⃣ Usar el comando `/enviar_evento` *. Este comando puedes usarlo desde cualquier lugar."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*2️⃣ Hablar directamente conmigo*. Abajo encontrarás un botón que te permite realizar la misma acción que con `/enviar_evento`."
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Enviar un evento",
+						"emoji": true
+					},
+					"value": "enviar",
+					"action_id": "enviar_evento",
+					"style": "primary"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "👀 Puedes ver otros comandos con `/task list`.\n❓Usa `/task help` para obtener ayuda."
+				}
+			]
+		}
+	]
+}
 
 export { vistasBienvenida };
