2fb35fa2269e2696d323b22a50eaa42473cbb976
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-13 02:37:03 +0100

    Heroku

--- src/controllers/slack.ts
+++ src/controllers/slack.ts
@@ -1,22 +1,25 @@
-import {service} from '../services/slackService';
+import { service } from '../services/slackService';
 import { viewBienvenida, viewEnviarEvento } from '../resources/views';
 import { ActionHandler, Respond } from '@slack/interactive-messages';
 
 function mencion(args: any) {
-    service.getWebClient().chat.postMessage({channel: args['channel'], text: 'Hola!'});
+    service.getWebClient().chat.postMessage({ channel: args['channel'], text: 'Hola!' });
 }
 
 function hablarConBot(args: any) {
-    service.getWebClient().chat.postMessage({channel: args['channel'], text: 'Hola! Soy el bot de gestión del Comité de Comunicación (ComCom). Para solicitar que difundamos un evento, utiliza el acceso directo /enviar_evento.'});
+    service.getWebClient().chat.postMessage({ channel: args['channel'], text: 'Hola! Soy el bot de gestión del Comité de Comunicación (ComCom). Para solicitar que difundamos un evento, utiliza el acceso directo /enviar_evento.' });
 }
 
 function bienvenida(args: any) {
-    service.getWebClient().views.publish({view: viewBienvenida, user_id: args['user']});
+    service.getWebClient().views.publish({ view: viewBienvenida, user_id: args['user'] });
 }
 
 function enviarEvento(payload: any, respond: Respond): any {
-    service.getWebClient().views.open({ trigger_id: payload['trigger_id'], view: viewEnviarEvento })
-    console.log(payload);
+    //service.getWebClient().views.open({ trigger_id: payload['trigger_id'], view: viewEnviarEvento });
+    respond({
+        text: "Thanks for your request, we'll process it and get back to you.",
+        response_type: "ephemeral"
+    }).catch((err) => console.error(err)).then((result) => console.log(result));
 }
 
 export { mencion, enviarEvento, hablarConBot, bienvenida };
