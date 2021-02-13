44e085a00280df1430c4dbd53fab050653e0535d
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-13 16:50:59 +0100

    Heroku

--- src/database/eventsDB.ts
+++ src/database/eventsDB.ts
@@ -5,6 +5,6 @@ var connection: Connection;
 createConnection({
     type: 'postgres',
     url: process.env.DATABASE_URL || ''
-}).then((con) => connection = con);
+}).then((con) => connection = con).catch((err) => console.error(err));
 
 export { connection };
