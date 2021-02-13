49dac4c9a3b81b814d5052af20d97e1ff017af1d
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-13 22:53:55 +0100

    Heroku

--- src/database/eventsDB.ts
+++ src/database/eventsDB.ts
@@ -6,7 +6,8 @@ createConnection({
     type: 'postgres',
     url: process.env.DATABASE_URL || '',
     ssl: { rejectUnauthorized: false },
-    logging: "all"
+    logging: "all",
+    logger: "simple-console"
 }).then((con) => {console.log("conectado a la BD"); connection = con;}).catch((err) => console.error(err));
 
 export { connection };
