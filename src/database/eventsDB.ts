329fb510debbb46e970dd6c773f6362915a0d2cf
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-14 17:51:20 +0100

    Heroku

--- src/database/eventsDB.ts
+++ src/database/eventsDB.ts
@@ -1,4 +1,5 @@
 import { Connection, createConnection } from 'typeorm';
+import { Event } from '../entity/Event';
 
 var connection: Connection;
 
@@ -8,7 +9,7 @@ createConnection({
     ssl: { rejectUnauthorized: false },
     logging: "all",
     logger: "simple-console",
-    dropSchema: true
+    entities: [ Event ]
 }).then((con) => {console.log("Conectado a la BD"); connection = con;}).catch((err) => console.error(err));
 
 export { connection };
