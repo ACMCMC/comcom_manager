fdce9373c97bf30b8c86e20705a7899eed4ecf39
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-14 20:23:27 +0100

    Heroku

--- src/resources/messageBlocks.ts
+++ src/resources/messageBlocks.ts
@@ -2,40 +2,38 @@ import { KnownBlock } from '@slack/web-api';
 
 const mensajeSolicitudPublicacion: KnownBlock[] = [
     {
-        "type": "header",
-        "text": {
-            "type": "plain_text",
-            "text": "New request"
-        }
-    },
-    {
-        "type": "section",
-        "fields": [
-            {
-                "type": "mrkdwn",
-                "text": "*Type:*\nPaid Time Off"
-            },
-            {
-                "type": "mrkdwn",
-                "text": "*Created by:*\n<example.com|Fred Enriquez>"
-            }
-        ]
-    },
-    {
-        "type": "section",
-        "fields": [
-            {
-                "type": "mrkdwn",
-                "text": "*When:*\nAug 10 - Aug 13"
-            }
-        ]
-    },
-    {
         "type": "section",
         "text": {
             "type": "mrkdwn",
-            "text": "<https://example.com|View request>"
+            "text": "¡Hola, canal! Soy el *Gestor de ComCom*. \n X me ha pedido publicar el evento _dasd_. Genial!"
         }
+    },
+    {
+        "type": "divider"
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Saber más sobre el proceso",
+                    "emoji": true
+                },
+                "value": "click_me_123"
+            },
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Contactar con ComCom",
+                    "emoji": true
+                },
+                "value": "click_me_123",
+                "url": "https://google.com"
+            }
+        ]
     }
 ];
 
