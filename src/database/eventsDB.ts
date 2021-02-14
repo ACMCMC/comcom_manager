f5d8589fc04914e780caaf87020e12e40594f392
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-02-14 17:29:23 +0100

    Heroku

--- src/database/eventsDB.ts
+++ src/database/eventsDB.ts
@@ -7,7 +7,8 @@ createConnection({
     url: process.env.DATABASE_URL || '',
     ssl: { rejectUnauthorized: false },
     logging: "all",
-    logger: "simple-console"
+    logger: "simple-console",
+    dropSchema: true
 }).then((con) => {console.log("Conectado a la BD"); connection = con;}).catch((err) => console.error(err));
 
 export { connection };
